3752ab4ad7f6bc78187eb59eba957166ea50337b
BSSE23027 <143418689+BSSE23027@users.noreply.github.com>
2025-12-19 18:20:12 +0500

    Update database connection to use environment variables

--- connection.php
+++ connection.php
@@ -1,7 +1,14 @@
 
-
 <?php
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASS");
+$database = getenv("DB_NAME");
+
 //change mysqli_connect(host_name,username, password); 
-$connection = mysqli_connect("localhost", "root", "");
-$db = mysqli_select_db($connection, 'demo');
+
+    $connection = mysqli_connect($host, $user, $password, $database);
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 ?>
